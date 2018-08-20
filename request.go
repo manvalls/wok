0efb3dc35d72578efdfaf5dd4943630908f477c3
Manuel Valls Fernández <manolo@vlrz.es>
2018-08-20 02:26:14 +0200

    Add some http wrappers to Request

--- request.go
+++ request.go
@@ -3,8 +3,10 @@ package wok
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/manvalls/way"
 	"github.com/manvalls/wit"
@@ -83,6 +85,26 @@ func (r Request) URLRedirect(statusCode int, params way.Params, route ...uint) w
 	r.ResponseWriter.Header().Set("Location", redirURL)
 	r.SetStatusCode(statusCode)
 	return wit.End
+}
+
+// MaxBytesReader limits the size of a reader
+func (r Request) MaxBytesReader(rc io.ReadCloser, n int64) io.ReadCloser {
+	return http.MaxBytesReader(r.ResponseWriter, rc, n)
+}
+
+// ServeContent replies to the request using the content in the provided ReadSeeker
+func (r Request) ServeContent(name string, modtime time.Time, content io.ReadSeeker) {
+	http.ServeContent(r.ResponseWriter, r.Request, name, modtime, content)
+}
+
+// ServeFile replies to the request with the contents of the named file or directory
+func (r Request) ServeFile(name string) {
+	http.ServeFile(r.ResponseWriter, r.Request, name)
+}
+
+// SetCookie adds a Set-Cookie header to the provided ResponseWriter's headers
+func (r Request) SetCookie(cookie *http.Cookie) {
+	http.SetCookie(r.ResponseWriter, cookie)
 }
 
 // Handler implements an HTTP handler which provides wok requests
