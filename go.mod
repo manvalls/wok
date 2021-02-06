376d6e3a304621ff9f3dff49bbffcfc336c08d35
Manuel Valls Fernández <manolo@vlrz.es>
2021-02-06 21:33:26 +0000

    Start major refactor

--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module github.com/manvalls/wok
 
 go 1.14
 
-require (
-	github.com/andybalholm/cascadia v1.2.0 // indirect
-	github.com/golang/gddo v0.0.0-20200611223618-a4829ef13274
-	github.com/gorilla/websocket v1.4.2
-	github.com/manvalls/way v0.0.0-20200627075720-9578b1181034
-	github.com/manvalls/wit v0.0.0-20190215154555-bf9dc158b462
-	github.com/oklog/ulid v1.3.1
-)
+require github.com/manvalls/wit v0.0.0-20201230125249-1acad31de893
---
+++ types.go
@@ -0,0 +1,68 @@
+package wok
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+
+	"github.com/manvalls/wit"
+)
+
+// Params holds the list of parameters for a given URL
+type Params = url.Values
+
+// Router computes the list of controllers that should run for a given URL or route
+type Router interface {
+	ResolveURL(r *http.Request, url string) (result RouteResult)
+	ResolveRoute(r *http.Request, route string, params map[string]string) (resolvedURL string, result RouteResult)
+}
+
+// RouteResult contains the resolved plan for a route
+type RouteResult struct {
+	Controllers []ControllerPlan
+	ReloadOn    []string
+}
+
+// ControllerPlan represents a controller to be ran
+type ControllerPlan struct {
+	Controller string
+	Method     string
+	Params
+
+	DependsOn       []string
+	RunAfter        []string
+	Batch           bool
+	Lazy            bool
+	Socket          bool
+	NeedsCleanup    bool
+	NeedsValidation bool
+}
+
+// ControllerRequest represents a request to run a controller
+type ControllerRequest interface {
+	Controller() string
+	Method() string
+	RunAfter() []string
+	Cleanup() bool
+	Validate() bool
+	Params() Params
+
+	SendDelta(delta wit.Delta)
+	ExposeURL(url string, routeResult RouteResult)
+	Redirect(url string, status int, routeResult RouteResult)
+	ExternalRedirect(url string, status int)
+	SetStatus(status int)
+	AddHeader(key string, values []string)
+	SetHeader(key string, values []string)
+	ReloadOn(events []string)
+	AbortOn(events []string)
+	Trigger(events []string)
+	Close()
+
+	context.Context
+}
+
+// App holds and runs the list of controllers that conform an application
+type App interface {
+	Run(r *http.Request, controllerRequests []ControllerRequest)
+}
